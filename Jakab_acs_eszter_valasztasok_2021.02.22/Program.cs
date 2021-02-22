0c22bcb76cbb9af6701cfc38c0551b24f87750ce
varzoltan <64203391+varzoltan@users.noreply.github.com>
2021-02-22 14:02:45 +0100

    Add project files.

---
+++ Jakab_acs_eszter_valasztasok_2021.02.22/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jakab_acs_eszter_valasztasok_2021._02._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
